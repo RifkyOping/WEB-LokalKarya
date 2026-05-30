f9caf9a5253373ebd66fe09674b14c8b5b448da3
latihanCommit <yobopi8143@inupup.com>
2026-05-30 21:07:37 +0800

    Fix: Force HTTPS untuk CSS di Vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 1. Tambahkan baris ini
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
+        * Register any application services.
+        */
     public function register(): void
     {
         //
     }
 
     /**
-     * Bootstrap any application services.
-     */
+        * Bootstrap any application services.
+        */
     public function boot(): void
     {
-        //
+        // 2. Paksa HTTPS jika aplikasi berjalan di server Vercel
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
